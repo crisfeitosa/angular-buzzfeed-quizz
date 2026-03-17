fe91b754cd3affd7ce03d733fdff0a3bf0fe65f0
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-03-17 11:12:18 -0300

    feat: update quizz component to use dynamic title and question display

--- src/app/components/quizz/quizz.ts
+++ src/app/components/quizz/quizz.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.css',
 })
 export class Quizz {
+  title: string = '';
 
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
 }
